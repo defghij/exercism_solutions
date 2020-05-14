d1513f75a005e9276b89cf231b3d51bd7741c35b
defghij <norrisc@protonmail.com>
2020-05-14 15:43:58 -0400

    Removed for loop in anagram exercise

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,21 +1,17 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut anagrams: HashSet<& str> = HashSet::new();
-
-    for poss_anagram in possible_anagrams.iter() {
-        if is_nontrivial_anagram(word, poss_anagram) {
-            anagrams.insert(poss_anagram);
-        }
-    }
-
+    let anagrams = possible_anagrams
+        .iter()
+        .filter(|x| is_nontrivial_anagram(word, x))
+        .copied()
+        .collect();
     anagrams
 }
 
 fn is_nontrivial_anagram(word_one: &str, word_two: &str) -> bool {
     to_sorted_vec(&word_one.to_lowercase()) == to_sorted_vec(&word_two.to_lowercase())
-    &&
-    word_one.to_lowercase() != word_two.to_lowercase()
+        && word_one.to_lowercase() != word_two.to_lowercase()
 }
 
 fn to_sorted_vec<'a>(word: &str) -> Vec<char> {
